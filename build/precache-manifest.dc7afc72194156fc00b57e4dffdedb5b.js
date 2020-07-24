0155025889ddd5a215357449e420c74dff01ccc7
Ronit Kumar Nayak <ronit@Ubuntu20.04>
2020-07-24 14:48:21 +0530

    Build Ready

---
+++ build/precache-manifest.dc7afc72194156fc00b57e4dffdedb5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed3d4b0ab012f39f545a82660865595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1ef01d4204787f0c14",
+    "url": "/static/css/main.576111f8.chunk.css"
+  },
+  {
+    "revision": "909738b0362fa36ffa39",
+    "url": "/static/js/2.a6b3005f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6b3005f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1ef01d4204787f0c14",
+    "url": "/static/js/main.41b6f6aa.chunk.js"
+  },
+  {
+    "revision": "9c94d117edcdf250e7f6",
+    "url": "/static/js/runtime-main.cfb71e88.js"
+  }
+]);
